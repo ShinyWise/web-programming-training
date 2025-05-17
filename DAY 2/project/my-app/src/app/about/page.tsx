c98a17023534497255abcb57117c189b55d82cf2
ShinyWise <franswinata06@gmail.com>
2025-05-17 19:12:42 +0700

    progress on making multipage portofolio web, simplefied the web structure

---
+++ DAY 2/project/my-app/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { div, h1 } from "motion/react-client";
+
+export default function about() {
+    return(
+        <div>
+            <h1>OMG THIS IS A YOUR ABOUT PAGE</h1>
+            <Button> HELLO????? </Button>
+        </div>
+    );
+}
